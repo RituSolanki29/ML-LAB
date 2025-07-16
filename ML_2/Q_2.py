3f74acfbf624ff3cc96b80c1565ec346b2515593
RituSolanki29 <ritusolanki1629@example.com>
2025-07-16 12:39:50 +0530

    A_2

--- ML_2/Q_2.py
+++ ML_2/Q_2.py
@@ -10,15 +10,14 @@ df = pd.read_excel('Lab Session Data.xlsx', sheet_name="Purchase data", engine='
 print("Data Preview:")
 print(df)
 
-
 # Step 1: Label as RICH (1) or POOR (0)
-df['Label'] = df['Payment (Rs)'].apply(lambda x: 1 if x > 200 else 0)
+df['Label'] = df['Payment'].apply(lambda x: 1 if x > 200 else 0)
 
 # Step 2: Define features (X) and target (y)
-X = df[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']]
+X = df[['Candies ', 'Mangoes ', 'Milk Packets ']]
 y = df['Label']
 
-# Step 3: Split data into train and test sets (e.g., 70% train, 30% test)
+# Step 3: Split data into train and test sets (7:3)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Step 4: Train Logistic Regression classifier
